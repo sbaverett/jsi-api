ae43af15a4898d61eb802adfbe5864ac13c14c09
J. Allen Tucker <j.allentucker@gmail.com>
2014-06-30 16:12:21 -0700

    clicking on employee name now also displays employees who report to clicked employee

--- app/scripts/model/employee.js
+++ app/scripts/model/employee.js
@@ -4,6 +4,8 @@ module.exports = function(App) {
 	App.Employee = DS.Model.extend({
   	name: DS.attr(),
   	role: DS.attr(),
-    boss: DS.belongsTo('employee', { async: true })
+    boss: DS.belongsTo('employee', { async: true, inverse: 'reports' }),
+    reports: DS.hasMany('employee', { async: true, inverse: 'boss' })
 	});
+
 };
