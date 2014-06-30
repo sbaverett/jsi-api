d73978c6f360a93d5855e5026e8ce9c2919cc76a
J. Allen Tucker <j.allentucker@gmail.com>
2014-06-30 15:14:09 -0700

    generates list of employees, clicking on employee name tells user boss name

--- app/scripts/model/employee.js
+++ app/scripts/model/employee.js
@@ -3,6 +3,7 @@
 module.exports = function(App) {
 	App.Employee = DS.Model.extend({
   	name: DS.attr(),
-  	role: DS.attr()
+  	role: DS.attr(),
+    boss: DS.belongsTo('employee', { async: true })
 	});
 };
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -3,6 +3,7 @@
 module.exports = function(App) {
 	App.Router.map(function() {
 		this.resource('employee');
+		this.route('boss', { path: 'employee/:employee_id'});
 	});
 
 	App.IndexRoute = Ember.Route.extend({
@@ -14,6 +15,12 @@ module.exports = function(App) {
 	App.EmployeeRoute = Ember.Route.extend({
 		model: function() {
 			return this.store.findAll('employee');
+		}
+	});
+
+	App.BossRoute = Ember.Route.extend({
+		model: function(params) {
+			return this.store.find('employee', params.employee_id);
 		}
 	});
 };
